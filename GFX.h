a105babd2fdd93d479c396cd9f4e1f6b9d4bb0e2
pyrohaz <harris.shallcross@gmail.com>
2015-02-03 23:13:44 +0000

    Update GFX.h

--- GFX.h
+++ GFX.h
@@ -17,7 +17,7 @@
  *www.hsel.co.uk
  *
  *The MIT License (MIT)
- *Copyright (c) 2014 Harris Shallcross
+ *Copyright (c) 2015 Harris Shallcross
  *
  *Permission is hereby granted, free of charge, to any person obtaining a copy
  *of this software and associated documentation files (the "Software"), to deal
